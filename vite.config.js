67eb75ea6b8ae72e7b74b2ce0b112187094a3dac
Thanaphiphat <thanaphiphat.ji@gmail.com>
2026-02-20 23:31:36 +0700

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-react-app/',   // 👈 สำคัญมากสำหรับ GitHub Pages
+
+  server: {
+    port: 5173,
+    proxy: {
+      '/gas': {
+        target: 'https://script.google.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) =>
+          path.replace(
+            /^\/gas/,
+            '/macros/s/AKfycby1YzD0g7VJq-NPMfulSu6re6Ikwh7HeU3uwl_OAIWXCadHvtAzT8oork-KJbwCdFvKmA/exec'
+          )
+      }
+    }
+  }
+})
